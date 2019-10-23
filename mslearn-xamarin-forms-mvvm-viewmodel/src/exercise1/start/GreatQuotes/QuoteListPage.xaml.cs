4bbab361e7b1b31f95dff102b6fb512d0dfff832
Rene Bentes Pinto <rene_bentes@hotmail.com>
2019-10-23 19:26:22 -0300

    Adiciona estudo sobre MVVM

---
+++ mslearn-xamarin-forms-mvvm-viewmodel/src/exercise1/start/GreatQuotes/QuoteListPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using GreatQuotes.ViewModels;
+
+namespace GreatQuotes
+{
+    public partial class QuoteListPage : ContentPage
+    {
+        public QuoteListPage()
+        {
+            BindingContext = App.MainViewModel;
+            InitializeComponent();
+        }
+
+        private void OnQuoteSelected(object sender, ItemTappedEventArgs e)
+        {
+            Navigation.PushAsync(new QuoteDetailPage(), true);
+        }
+    }
+}
---
+++ mslearn-xamarin-forms-mvvm-viewmodel/src/exercise1/start/GreatQuotes/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GreatQuotes.Data;
+using GreatQuotes.Infrastructure;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace GreatQuotes.ViewModels
+{
+    public class MainViewModel : SimpleViewModel
+    {
+        private QuoteViewModel _selectedQuote;
+
+        public MainViewModel()
+        {
+            Quotes = new ObservableCollection<QuoteViewModel>(
+                QuoteManager.Load()
+                            .Select(q => new QuoteViewModel(q)));
+        }
+
+        public IList<QuoteViewModel> Quotes { get; private set; }
+
+        public QuoteViewModel SelectedQuote
+        {
+            get { return _selectedQuote; }
+            set { SetPropertyValue(ref _selectedQuote, value); }
+        }
+    }
+}
