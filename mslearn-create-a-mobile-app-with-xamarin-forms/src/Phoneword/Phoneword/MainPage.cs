d826bcef8762ec74dfe167202eddc5fb9fdf619b
Rene Bentes Pinto <renebentes@yahoo.com.br>
2020-05-16 15:48:26 -0300

    Cria interface do usuário

--- mslearn-create-a-mobile-app-with-xamarin-forms/src/Phoneword/Phoneword/MainPage.cs
+++ mslearn-create-a-mobile-app-with-xamarin-forms/src/Phoneword/Phoneword/MainPage.cs
@@ -4,5 +4,36 @@ namespace Phoneword
 {
     public class MainPage : ContentPage
     {
+        private Button callButton;
+        private Entry phoneNumberText;
+        private Button translateButton;
+        private string translatedNumber;
+
+        public MainPage()
+        {
+            Padding = new Thickness(20);
+
+            StackLayout panel = new StackLayout() { Spacing = 15 };
+
+            panel.Children.Add(new Label { Text = "Enter a Phoneword", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) });
+
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN",
+            });
+
+            panel.Children.Add(translateButton = new Button
+            {
+                Text = "Translate"
+            });
+
+            panel.Children.Add(callButton = new Button
+            {
+                Text = "Call",
+                IsEnabled = false,
+            });
+
+            Content = panel;
+        }
     }
 }
